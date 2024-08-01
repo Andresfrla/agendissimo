22916cbae042c135a8702c7b51015232afdace06
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-07-31 19:36:36 -0600

    add the connection to mongo

---
+++ app/_utils/db.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache;
+}
+
+let cached: MongooseCache;
+
+if (!global.mongoose) {
+  global.mongoose = { conn: null, promise: null };
+}
+cached = global.mongoose;
+
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectToDatabase;

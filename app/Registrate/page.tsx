088e283eafdc29885b3df8a9fddd65dca2e2d011
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-11-30 13:26:19 -0600

    and styling to the register page

--- app/Registrate/page.tsx
+++ app/Registrate/page.tsx
@@ -5,6 +5,7 @@ import Navbar from "../_components/_Navbar/page";
 import Jenn from "@/public/Jenn-1.png";
 import { Input, Stack, Button } from "@chakra-ui/react";
 import { ChangeEvent, useState, FormEvent } from "react";
+import Footer from "../_components/_Footer/page";
 
 const SignIn = () => {
     const [formData, setFormData] = useState({
@@ -31,11 +32,11 @@ const SignIn = () => {
     return (
         <div>
             <Navbar />
-            <div className="flex items-center justify-center text-[#010101]">
+            <div className="flex items-center justify-center text-[#010101] mb-40">
                 <div className="bg-[#FAF7F6] w-2/3 flex items-center flex-col mt-40 rounded-lg">
                     <h3 className="text-3xl font-bold mb-10 mt-10">Regístrate</h3>    
                     <div className="flex justify-around mb-10 w-full px-10">
-                        <div className="w-2/3 flex">
+                        <div className="">
                             <form onSubmit={handleSubmit}>
                                 <Stack spacing={3}>
                                     <h4>Correo Electrónico:</h4>                
@@ -45,7 +46,7 @@ const SignIn = () => {
                                         name="email" 
                                         value={formData.email} 
                                         onChange={handleChange}
-                                        className="p-2 rounded-xl my-4 w-full" 
+                                        className="p-2 rounded-xl my-4 w-96" 
                                     />
                                     
                                     <h4>Nombre Completo:</h4>
@@ -61,7 +62,7 @@ const SignIn = () => {
                                     <h4>Teléfono:</h4>
                                     <Input 
                                         type="tel" 
-                                        placeholder="1234567890" 
+                                        placeholder="55550055" 
                                         name="phone" 
                                         value={formData.phone} 
                                         onChange={handleChange}
@@ -86,17 +87,18 @@ const SignIn = () => {
                                         onChange={handleChange}
                                         className="p-2 rounded-xl my-4 w-full" 
                                     />
-
-                                    <Button type="submit" colorScheme="teal" className="mt-4 w-full">
+                                    <div className="flex justify-center">
+                                    <Button type="submit" colorScheme="teal" className="mt-4 w-1/2 rounded-md p-2 bg-[#010101] text-[#e7d9d9]">
                                         Registrarse
                                     </Button>
+                                    </div>
                                 </Stack>
                             </form>
                         </div>
                         <div className="w-1/3">
                             <Image
                                 src={Jenn}
-                                width={300}
+                                width={370}
                                 height={500}
                                 alt="Jenn"
                                 style={{
@@ -107,6 +109,7 @@ const SignIn = () => {
                     </div>
                 </div>
             </div>
+            <Footer/>
         </div>
     );
 }
